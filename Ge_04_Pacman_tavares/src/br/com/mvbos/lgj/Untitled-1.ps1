e58c2c7e16062106db7eadc34143e5b63b1f4aad
rogtavares <rogerio@almeidatavares.com.br>
2025-07-01 15:29:16 -0300

    commint 2 bug2

---
+++ Ge_04_Pacman_tavares/src/br/com/mvbos/lgj/Untitled-1.ps1
@@ -0,0 +1,6 @@
+
+
+cd "C:\Users\Cliente\Documents\GIT REPOSITORIOS\JAVA FLIPERAMA\Ge_04_Pacman_tavares\src"
+javac -encoding UTF-8 -d ../bin br/com/mvbos/lgj/Jogo.java br/com/mvbos/lgj/InicioCenario.java br/com/mvbos/lgj/JogoCenario.java br/com/mvbos/lgj/Nivel.java br/com/mvbos/lgj/Pizza.java br/com/mvbos/lgj/base/CenarioPadrao.java br/com/mvbos/lgj/base/Elemento.java br/com/mvbos/lgj/base/Menu.java br/com/mvbos/lgj/base/Texto.java br/com/mvbos/lgj/base/Util.java
+cd ..
+java -cp bin br.com.mvbos.lgj.Jogo
